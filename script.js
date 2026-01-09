a98e2b64e235e98c99a51ae0ba5f9bf97fef7f34
Jon <126274870+jonmc89@users.noreply.github.com>
2026-01-09 17:57:25 +0000

    Body (main):
    Tablet view setup, form validation for tablet view added.
    
    Jon M

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function isValidEmail(email) {
     return regex.test(email);
 }
 
+// Mobile View
+
 document.getElementById("form").addEventListener("submit", function (event) {
     const emailInput = document.getElementById("emailInput");
     const errorMsg = document.getElementById("errorMsg");
@@ -27,3 +29,33 @@ document.getElementById("form").addEventListener("submit", function (event) {
         }, 2000);
     }
 });
+
+// Tablet & Desktop View
+
+document
+    .getElementById("formDesktop")
+    .addEventListener("submit", function (event) {
+        const emailInputDesktop = document.getElementById("emailInputDesktop");
+        const errorMsgDesktop = document.getElementById("errorMsgDesktop");
+        const successMsg = document.getElementById("successMsg");
+        const formDesktop = document.getElementById("formDesktopContainer");
+
+        if (!isValidEmail(emailInputDesktop.value)) {
+            event.preventDefault();
+            formDesktopContainer.classList.add("input-invalid");
+            errorMsgDesktop.classList.add("error-msg-reveal");
+
+            setTimeout(() => {
+                formDesktopContainer.classList.remove("input-invalid");
+                errorMsgDesktop.classList.remove("error-msg-reveal");
+            }, 3000);
+        } else {
+            event.preventDefault();
+            successMsg.classList.add("success-message-reveal");
+            emailInputDesktop.value = "";
+
+            setTimeout(() => {
+                successMsg.classList.remove("success-message-reveal");
+            }, 2000);
+        }
+    });
